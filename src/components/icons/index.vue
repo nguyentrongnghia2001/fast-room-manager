6fa13148465aadab62af6d4e7e61ffcf6214025f
nguyentrongnghia2001 <nghianhodoi@gmail.com>
2025-11-14 21:16:15 +0700

    refactor(icon): using index icon vue

---
+++ src/components/icons/index.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { computed, defineAsyncComponent } from 'vue'
+
+const props = defineProps({
+  name: {
+    type: String,
+    default: 'IconTooling',
+    required: true
+  }
+})
+const iconComponent = computed(() =>
+  defineAsyncComponent(() =>
+    import(`/src/components/icons/${props.name}.vue`)
+  )
+)
+
+</script>
+
+<template>
+  <component :is="iconComponent" />
+</template>
--- src/views/DocumentsView.vue
+++ src/views/DocumentsView.vue
@@ -1,12 +1,7 @@
 <script setup lang="ts">
 import AlertBase from '@/components/base/alert/AlertBase.vue'
 import ButtonBase from '@/components/base/button/ButtonBase.vue'
-import InputTextBase from '@/components/base/input/InputTextBase.vue'
-import InputSelectBase from '@/components/base/input/InputSelectBase.vue'
-import InputNumberBase from '@/components/base/input/InputNumberBase.vue'
-import InputTextareaBase from '@/components/base/input/InputTextareaBase.vue'
-import IconBack from '@/components/icons/IconBack.vue'
-import IconSpin from '@/components/icons/IconSpin.vue'
+import Icons from '@/components/icons/index.vue'
 </script>
 <template>
   <div class="grid grid-cols-1 gap-4">
@@ -23,5 +18,10 @@ import IconSpin from '@/components/icons/IconSpin.vue'
     <p class="text-sm text-primary-500">
       This is a paragraph.
     </p>
+    <div class="bg-primary-500 w-10 h-auto flex-col flex items-center justify-center">
+      <Icons name="IconDocumentation" /> 
+      <Icons name="IconBack" /> 
+      <Icons name="IconDocumentation" /> 
+    </div>
   </div>
 </template>
